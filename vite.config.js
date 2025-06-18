ff2c9d3cd9845b0995530154a407b5c376c6a508
markpomerant <115945922+markpomerant@users.noreply.github.com>
2025-06-18 21:28:31 +0000

    Add GitHub Actions workflow for deployment and set base path in Vite config

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,7 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/sudoku/',
   test: {
     globals: true,
     environment: 'jsdom',
